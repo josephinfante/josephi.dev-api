95d07c728b102cfb202b9c1dbf76a761270c3602
Joseph Infante <josephinfante.y@gmail.com>
2026-01-31 11:10:17 -0500

    feat(cache): add redis cache and pubsub support for presence

--- src/shared/cache/index.ts
+++ src/shared/cache/index.ts
@@ -1,3 +1,3 @@
 export { RedisClient } from '@cache/redis.client';
 export { CacheService } from '@cache/cache.service';
-export { CacheKeys } from '@cache/cache.keys';
+export { PRESENCE_KEYS } from '@cache/cache.keys';
